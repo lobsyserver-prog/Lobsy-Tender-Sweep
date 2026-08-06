6071b70d7821817a0aace6c57d47841afa01937e
lobsyserver-prog <lobsyserver@gmail.com>
2026-08-06 10:18:59 +0000

    Remove redundant user seeding code from seed.js

--- backend/seed.js
+++ backend/seed.js
@@ -8,13 +8,3 @@ const user = DB.findOrCreateUser({
 })
 
 console.log('Seeded user:', user)
-const DB = require('./database')
-
-const user = DB.findOrCreateUser({
-  id: 'testuser1',
-  username: 'tester',
-  displayName: 'Test User',
-  profileUrl: 'https://github.com/tester'
-})
-
-console.log('Seeded user:', user)

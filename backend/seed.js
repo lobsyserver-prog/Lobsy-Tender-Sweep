abf8989719a305d627435db83ae147cafd2ecf59
lobsyserver-prog <lobsyserver@gmail.com>
2026-08-06 10:16:56 +0000

    Add seed script for user creation in the database

---
+++ backend/seed.js
@@ -0,0 +1,20 @@
+const DB = require('./database')
+
+const user = DB.findOrCreateUser({
+  id: 'testuser1',
+  username: 'tester',
+  displayName: 'Test User',
+  profileUrl: 'https://github.com/tester'
+})
+
+console.log('Seeded user:', user)
+const DB = require('./database')
+
+const user = DB.findOrCreateUser({
+  id: 'testuser1',
+  username: 'tester',
+  displayName: 'Test User',
+  profileUrl: 'https://github.com/tester'
+})
+
+console.log('Seeded user:', user)
